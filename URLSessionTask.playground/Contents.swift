835fc7e72777f50ebe0706c2c5c3003288842907
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-05-11 13:29:21 +0600

    Implemented Marvel API.

--- URLSessionTask.playground/Contents.swift
+++ URLSessionTask.playground/Contents.swift
@@ -1,10 +1,26 @@
 import Foundation
+import CryptoKit
+
+let publicKey = "d547b70b9ceb80d73036b2fcbb140842"
+let privateKey = "136ffee995063d262a6512586f44f58445e3f6f3"
+var hash: String {
+    MD5(string: ts + privateKey + publicKey)
+}
+private let ts = String(Int.random(in: 1...99))
+
+func MD5(string: String) -> String {
+    let digest = Insecure.MD5.hash(data: Data(string.utf8))
+
+    return digest.map {
+        String(format: "%02hhx", $0)
+    }.joined()
+}
 
 func getData(url: String) {
     let url = URL(string: url)
     guard let url = url else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
-        if error != nil {
+        if let error = error {
             print("[error] \(String(describing: error))")
         }
 
@@ -17,4 +33,4 @@ func getData(url: String) {
     }.resume()
 }
 
-getData(url: "https://api.breakingbadquotes.xyz/v1/quotes")
+getData(url: "https://gateway.marvel.com/v1/public/characters?ts=\(ts)&apikey=\(publicKey)&hash=\(hash)")
